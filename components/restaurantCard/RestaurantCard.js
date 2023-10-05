8db56151dd19b0309a3e7d2aebb7fa2a93627cc2
Mehul Kumawat <mehul.kumawat@antrazal.com>
2023-10-05 18:43:59 +0530

    FEAT- fetch api data

--- components/restaurantCard/RestaurantCard.js
+++ components/restaurantCard/RestaurantCard.js
@@ -1,10 +1,11 @@
 import "./restaurantCard.css";
+import { CDN_URL } from "../../utils/utils";
 const RestaurantCard = (props) => {
     const {resData} = props;
     const {cloudinaryImageId, cuisines, areaName , avgRating, name} = resData?.info;
     return (
         <div className="res-card">
-                <img className="res-img" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +cloudinaryImageId} />
+                <img className="res-img" src={CDN_URL +cloudinaryImageId} />
             <div className="res-details">
                 <div className="res-name overflow">
                     {name}
