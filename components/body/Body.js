3125507f9c1808a6fbe19d1ba90e2df9f651aa03
Mehul Kumawat <mehul.kumawat@antrazal.com>
2023-10-06 11:12:52 +0530

    FEAT- shimmer UI

--- components/body/Body.js
+++ components/body/Body.js
@@ -1,19 +1,35 @@
 import RestaurantCard from "../restaurantCard/RestaurantCard";
+import Shimmer from "../shimmer/Shimmer";
 import "./body.css";
-import { data } from "../../utils/mockData.js";
-import { useState } from "react";
+// import { data } from "../../utils/mockData.js";
+import { useEffect, useState } from "react";
 const Body = () => {
-    let resList = data?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-    const [resListt, setResList] = useState(resList);
+    // let resList = data?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+    const [resList, setResList] = useState([]);
+    useEffect(()=>{
+      // const data = fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.9166595&lng=75.7960106&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      // const json = data.json();
+      // setResList(json?.data?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      console.log("hello effect");
+      fetchData();
+    },[])
 
+    const fetchData = async () => {
+      const data = await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.9166595&lng=75.7960106&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      const json = await data.json();
+      await setResList(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      // console.log(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    };
+    
 
+    
 
-  return (
+  return resList.length === 0? <Shimmer/> : (
     <div className="body">
       <div className="search">
         <div className="search-text">
           <button className="filter-btn" onClick={() => {
-                setResList(resList.filter((res) => res.info.avgRating>4));
+                setResList(resList.filter((res) => res.info.avgRating>5));
           }}>
             Top Rated Restraunts
           </button>
@@ -21,7 +37,7 @@ const Body = () => {
         {/* <div className="search-input"><input /></div> */}
       </div>
       <div className="res-container">
-        {resListt.map((restaurant) => (
+        {resList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
---
+++ components/shimmer/ShimmerCard.js
@@ -0,0 +1,15 @@
+import "./shimmerCard.css"
+const ShimmerCard = () => {
+    return (
+        <div className="shimmer-res-card">
+            <div className="shimmer-res-img"></div>
+            <div className="shimmer-res-details">
+                <div className="shimmer-res-name"></div>
+                <div className="shimmer-res-name2"></div>
+                <div className="shimmer-res-name2"></div>
+                {/* <div className="res-name 3"></div> */}
+            </div>
+        </div>
+    )
+}
+export default ShimmerCard;
